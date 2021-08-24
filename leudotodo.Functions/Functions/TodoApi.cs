ba001e06e79378e7178254eb40d7239acf8d4f73
LeudoToure <68364692+LeudoToure@users.noreply.github.com>
2021-08-24 17:54:09 -0500

    Get

--- leudotodo.Functions/Functions/TodoApi.cs
+++ leudotodo.Functions/Functions/TodoApi.cs
@@ -108,6 +108,59 @@ namespace leudotodo.Functions.Functions
             });
 
         }
+
+
+        [FunctionName(nameof(GetAllTodos))]
+        public static async Task<IActionResult> GetAllTodos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")] HttpRequest req,
+            [Table("todo", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            log.LogInformation("Get all todos received");
+
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>();
+            TableQuerySegment<TodoEntity> todos = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+
+            string message = "Retrieved all todos";
+            log.LogInformation(message);
+            return new OkObjectResult(new Responses
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todos
+            });
+
+        }
+
+        [FunctionName(nameof(GetTodoById))]
+        public static IActionResult GetTodoById(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req,
+        [Table("todo", "TODO", "{id}", Connection = "AzureWebJobsStorage")] TodoEntity todoEntity,
+        string id,
+        ILogger log)
+        {
+            log.LogInformation($"Get atodo by id: {id} received");
+
+            if(todoEntity == null)
+            {
+                return new BadRequestObjectResult(new Responses
+                {
+                    IsSuccess = false,
+                    Message = "Todo no found"
+                });
+            }
+            string message = $"Todo: {todoEntity.RowKey}, retrieved";
+            log.LogInformation(message);
+
+
+            return new OkObjectResult(new Responses
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+
+        }
     }
 
 }
