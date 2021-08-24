cac104ffe20c0ead6b17c883116fa26f02335b65
LeudoToure <68364692+LeudoToure@users.noreply.github.com>
2021-08-24 17:35:12 -0500

    Update todo

---
+++ leudotodo.Common/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace leudotodo.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreateTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
